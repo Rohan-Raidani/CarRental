771a30052d3c95744b61ce6bdf5ee77d4818610d
Rohan-Raidani <raidanirohan409@gmail.com>
2024-10-03 10:07:39 +0530

    added more styles

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // const API_URL = "http://localhost:8000"
-// const API_URL = import.meta.env.VITE_API_URL
-const API_URL = "https://carrental-tefk.onrender.com"
+const API_URL = import.meta.env.VITE_API_URL
+// const API_URL = "https://carrental-tefk.onrender.com"
 
 // Adding car
 
